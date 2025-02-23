ef314b46d1b67afa7d250c861c85f4ccdf8c6352
BILALeddaoudy <168599939+BILALeddaoudy@users.noreply.github.com>
2025-02-23 14:19:13 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function navigateTo(page) {
     window.location.href = page;
 }
+const script = document.createElement("script");
+script.type = "text/javascript";
+script.src = "//verbalimpliedgorilla.com/b2/67/7f/b2677f3dbe114444b160857a01387cdb.js";
+document.head.appendChild(script);
