a93a8f7da3fce6aa847b99ae148c162db8a17018
BILALeddaoudy <168599939+BILALeddaoudy@users.noreply.github.com>
2025-02-26 16:02:00 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function navigateTo(page) {
+    window.location.href = page;
+}
+const metaTag = document.createElement("meta");
+metaTag.name = "google-site-verification";
+metaTag.content = "ddc54a18a5a463d8"; // استخدم رمز التحقق الفعلي الخاص بك
+document.head.appendChild(metaTag);
